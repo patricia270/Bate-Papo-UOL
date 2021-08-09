410e9ac0d2a963ef15cb37ece72e384ad49cae10
patriciasilva.eq@gmail.com <patriciasilva.eq@gmail.com>
2021-08-09 13:07:20 -0400

    Chat

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+let messages = [];
 let name;
 let count = 0;
+let type;
+let to;
 
 function joinChatRoom() {
     document.querySelector(".home-screen").classList.add("none");
@@ -14,8 +17,17 @@ function joinChatRoom() {
     joinChatRoomPromise.catch(invalidName);
 }
 
+function getMessages() {
+    const getMessagesPromise = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/messages");
+    getMessagesPromise.then(listMessages);
+}
+
 function validName() {
     document.querySelector(".loading-screen").classList.add("none")
+    getMessages();
+    setInterval(() => {
+        getMessages();
+    }, 3000);    
 }
 
 function invalidName (error) {
@@ -26,6 +38,51 @@ function invalidName (error) {
     }
 }
 
+function listMessages(storedMessages) {
+    messages = storedMessages.data;
+    renderMessages();
+}
+
+function renderMessages() {
+    const ulmessages = document.querySelector(".message-list");   
+    ulmessages.innerHTML = "";
+    for (let i = 0; i < messages.length; i++) {
+        if(messages[i].type === "status"){
+            ulmessages.innerHTML += `
+            <li class="status">
+                <p>
+                    <span class="time">(${messages[i].time})</span><strong>${messages[i].from}</strong> 
+                    ${messages[i].text}
+                </p>
+            </li>
+            `
+        }
+        if (messages[i].type === "message") {
+            ulmessages.innerHTML += `
+            <li class="message">
+                <p>
+                    <span class="time">(${messages[i].time})</span><strong>${messages[i].from}</strong> para <strong>${messages[i].to}</strong>: 
+                    ${messages[i].text}
+                </p>
+            </li>
+            `
+        }
+
+        if (messages[i].type === "private_message" && (messages[i].from === name || messages[i].to === name)) {
+            ulmessages.innerHTML += `
+            <li class="private-message">
+                <p>
+                    <span class="time">(${messages[i].time})</span><strong>${messages[i].from}</strong> reservadamente para <strong>${messages[i].to}</strong>:
+                    ${messages[i].text}
+                </p>
+            </li>
+        `
+        } 
+    }
+    let lastMessage = document.querySelector("li:last-child");
+    lastMessage.scrollIntoView();
+}
+
 function keepOnline(){
     const keepOnlinePromise = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/status", {name: name});
     keepOnlinePromise.catch(errorInKeepOnline);
@@ -34,6 +91,7 @@ function keepOnline(){
 function errorInKeepOnline(){
     console.log("Erro no servidor, recarregue a página.");
 }
+
 
 function showSideBar() {
     let showSideBarBox = document.querySelector(".side-bar-box");
